07374c0718863d0441a7ba4807dfde212cab0cdd
Dodomix <domi.zigrovic@gmail.com>
2017-01-07 21:54:43 +0100

    Dodana verzija Ascona sa dužim ključem

--- Programska implementacija/TestLib/Algorithms/Ascon256/ascon256_encrypt.c
+++ Programska implementacija/TestLib/Algorithms/Ascon256/ascon256_encrypt.c
@@ -7,14 +7,14 @@ typedef unsigned char u8;
 typedef unsigned long long u64;
 typedef long long i64;
 
-//#define PRINTSTATE
+//#define printstate2
 //#define PRINTWORDS
 #define size (320/8)
 #define rate (64/8)
 
 
-void printstate(char* text, u8* S) {
-#ifdef PRINTSTATE
+void printstate2(char* text, u8* S) {
+#ifdef printstate2
   int i;
   printf("%s\n", text);
   for (i = 0; i < 40; ++i)
@@ -23,7 +23,7 @@ void printstate(char* text, u8* S) {
 #endif
 }
 
-void printwords(char* text, u64 x0, u64 x1, u64 x2, u64 x3, u64 x4) {
+void printwords2(char* text, u64 x0, u64 x1, u64 x2, u64 x3, u64 x4) {
 #ifdef PRINTWORDS
   int i;
   printf("%s\n", text);
@@ -37,33 +37,33 @@ void printwords(char* text, u64 x0, u64 x1, u64 x2, u64 x3, u64 x4) {
 
 #define ROTR(x,n) (((x)>>(n))|((x)<<(64-(n))))
 
-void load64(u64* x, u8* S) {
+void _load64(u64* x, u8* S) {
   int i;
   *x = 0;
   for (i = 0; i < 8; ++i)
     *x |= ((u64) S[i]) << (56 - i * 8);
 }
 
-void store64(u8* S, u64 x) {
+void _store64(u8* S, u64 x) {
   int i;
   for (i = 0; i < 8; ++i)
     S[i] = (u8) (x >> (56 - i * 8));
 }
 
-void permutation(u8* S, int start, int rounds) {
+void permutation2(u8* S, int start, int rounds) {
   int i;
   u64 x0, x1, x2, x3, x4;
   u64 t0, t1, t2, t3, t4;
-  load64(&x0, S + 0);
-  load64(&x1, S + 8);
-  load64(&x2, S + 16);
-  load64(&x3, S + 24);
-  load64(&x4, S + 32);
-  printwords(" permutation input:", x0, x1, x2, x3, x4);
+  _load64(&x0, S + 0);
+  _load64(&x1, S + 8);
+  _load64(&x2, S + 16);
+  _load64(&x3, S + 24);
+  _load64(&x4, S + 32);
+  printwords2(" permutation input:", x0, x1, x2, x3, x4);
   for (i = start; i < rounds + start; ++i) {
     // addition of round constant
     x2 ^= ((0xfull - i) << 4) | i;
-    printwords(" addition of round constant:", x0, x1, x2, x3, x4);
+    printwords2(" addition of round constant:", x0, x1, x2, x3, x4);
     // substitution layer
     x0 ^= x4;    x4 ^= x3;    x2 ^= x1;
     t0  = x0;    t1  = x1;    t2  = x2;    t3  = x3;    t4  = x4;
@@ -71,23 +71,23 @@ void permutation(u8* S, int start, int rounds) {
     t0 &= x1;    t1 &= x2;    t2 &= x3;    t3 &= x4;    t4 &= x0;
     x0 ^= t1;    x1 ^= t2;    x2 ^= t3;    x3 ^= t4;    x4 ^= t0;
     x1 ^= x0;    x0 ^= x4;    x3 ^= x2;    x2 =~ x2;
-    printwords(" substitution layer:", x0, x1, x2, x3, x4);
+    printwords2(" substitution layer:", x0, x1, x2, x3, x4);
     // linear diffusion layer
     x0 ^= ROTR(x0, 19) ^ ROTR(x0, 28);
     x1 ^= ROTR(x1, 61) ^ ROTR(x1, 39);
     x2 ^= ROTR(x2,  1) ^ ROTR(x2,  6);
     x3 ^= ROTR(x3, 10) ^ ROTR(x3, 17);
     x4 ^= ROTR(x4,  7) ^ ROTR(x4, 41);
-    printwords(" linear diffusion layer:", x0, x1, x2, x3, x4);
+    printwords2(" linear diffusion layer:", x0, x1, x2, x3, x4);
   }
-  store64(S + 0, x0);
-  store64(S + 8, x1);
-  store64(S + 16, x2);
-  store64(S + 24, x3);
-  store64(S + 32, x4);
+  _store64(S + 0, x0);
+  _store64(S + 8, x1);
+  _store64(S + 16, x2);
+  _store64(S + 24, x3);
+  _store64(S + 32, x4);
 }
 
-int ascon_encrypt(
+int ascon256_encrypt(
     unsigned char *c, unsigned long long *clen,
     const unsigned char *m, unsigned long long mlen,
     const unsigned char *ad, unsigned long long adlen,
@@ -139,22 +139,22 @@ int ascon_encrypt(
     S[size - 2 * klen + i] = k[i];
   for (i = 0; i < klen; ++i)
     S[size - klen + i] = npub[i];
-  printstate("initial value:", S);
-  permutation(S, 12 - a, a);
+  printstate2("initial value:", S);
+  permutation2(S, 12 - a, a);
   for (i = 0; i < klen; ++i)
     S[rate + klen + i] ^= k[i];
-  printstate("initialization:", S);
+  printstate2("initialization:", S);
 
   // process associated data
   if (adlen != 0) {
     for (i = 0; i < s; ++i) {
       for (j = 0; j < rate; ++j)
         S[j] ^= A[i * rate + j];
-      permutation(S, 12 - b, b);
+      permutation2(S, 12 - b, b);
     }
   }
   S[size - 1] ^= 1;
-  printstate("process associated data:", S);
+  printstate2("process associated data:", S);
 
   // process plaintext
   for (i = 0; i < t - 1; ++i) {
@@ -162,21 +162,21 @@ int ascon_encrypt(
       S[j] ^= M[i * rate + j];
       c[i * rate + j] = S[j];
     }
-    permutation(S, 12 - b, b);
+    permutation2(S, 12 - b, b);
   }
   for (j = 0; j < rate; ++j)
     S[j] ^= M[(t - 1) * rate + j];
   for (j = 0; j < l; ++j)
     c[(t - 1) * rate + j] = S[j];
-  printstate("process plaintext:", S);
+  printstate2("process plaintext:", S);
 
   // finalization
   for (i = 0; i < klen; ++i)
     S[rate + i] ^= k[i];
-  permutation(S, 12 - a, a);
+  permutation2(S, 12 - a, a);
   for (i = 0; i < klen; ++i)
     S[rate + klen + i] ^= k[i];
-  printstate("finalization:", S);
+  printstate2("finalization:", S);
 
   // return tag
   for (i = 0; i < klen; ++i)
@@ -188,7 +188,7 @@ int ascon_encrypt(
   return 0;
 }
 
-int ascon_decrypt(
+int ascon256_decrypt(
     unsigned char *m, unsigned long long *mlen,
     unsigned char *nsec,
     const unsigned char *c, unsigned long long clen,
@@ -239,22 +239,22 @@ int ascon_decrypt(
     S[size - 2 * klen + i] = k[i];
   for (i = 0; i < klen; ++i)
     S[size - klen + i] = npub[i];
-  printstate("initial value:", S);
-  permutation(S, 12 - a, a);
+  printstate2("initial value:", S);
+  permutation2(S, 12 - a, a);
   for (i = 0; i < klen; ++i)
     S[rate + klen + i] ^= k[i];
-  printstate("initialization:", S);
+  printstate2("initialization:", S);
 
   // process associated data
   if (adlen) {
     for (i = 0; i < s; ++i) {
       for (j = 0; j < rate; ++j)
         S[j] ^= A[i * rate + j];
-      permutation(S, 12 - b, b);
+      permutation2(S, 12 - b, b);
     }
   }
   S[size - 1] ^= 1;
-  printstate("process associated data:", S);
+  printstate2("process associated data:", S);
 
   // process plaintext
   for (i = 0; i < t - 1; ++i) {
@@ -262,22 +262,22 @@ int ascon_decrypt(
       M[i * rate + j] = S[j] ^ c[i * rate + j];
       S[j] = c[i * rate + j];
     }
-    permutation(S, 12 - b, b);
+    permutation2(S, 12 - b, b);
   }
   for (j = 0; j < l; ++j)
     M[(t - 1) * rate + j] = S[j] ^ c[(t - 1) * rate + j];
   for (j = 0; j < l; ++j)
     S[j] = c[(t - 1) * rate + j];
   S[l] ^= 0x80;
-  printstate("process plaintext:", S);
+  printstate2("process plaintext:", S);
 
   // finalization
   for (i = 0; i < klen; ++i)
     S[rate + i] ^= k[i];
-  permutation(S, 12 - a, a);
+  permutation2(S, 12 - a, a);
   for (i = 0; i < klen; ++i)
     S[rate + klen + i] ^= k[i];
-  printstate("finalization:", S);
+  printstate2("finalization:", S);
 
   // return -1 if verification fails
   for (i = 0; i < klen; ++i)
